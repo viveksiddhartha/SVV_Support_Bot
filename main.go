b4b7a945fa171df3310e65bcabfcf548906ab7b7
Cody <siddelhi1@gmail.com>
2021-05-08 01:39:42 +0530

    Updated timeout setting

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	//u.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(u)
 
