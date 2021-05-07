4bad148bc837062c255b7cd55e0f2df30f80fe56
Cody <siddelhi1@gmail.com>
2021-05-08 00:58:11 +0530

    New Checkin

--- main.go
+++ main.go
@@ -1,125 +1,36 @@
 package main
 
 import (
-	"GoBotTel/types"
-	"GoBotTel/utils"
-	"encoding/json"
 	"log"
-	"net/http"
-	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func handler(res http.ResponseWriter, r *http.Request) {
-	update, err := utils.ParseTelegramUpdate(r)
-	if err != nil {
-		log.Printf("error parsing update, %s", err.Error())
-		return
-	}
-	log.Printf(update.Message.Text)
-	log.Println(update.CallbackQuery.Data)
-
-	userCmd, err := types.ParseCommand(update.Message.Text)
-	callbackData := update.CallbackQuery.Data
-
-	log.Println(userCmd)
-	// TODO Parse Arguments of the command too
-
-	var output string
-	var keyboard []byte = nil
-
-	if err != nil && callbackData == "" {
-		output = "Sorry you entered the wrong command. Here are the list of supported commands \n"
-		for command, desc := range types.Commands {
-			output += command + " : " + desc + "\n"
-		}
-	}
-
-	// Check if there is a callback from an inline button
-	if callbackData == "GN" {
-		output, err = utils.GetNewsForResponse("the-times-of-india")
-		log.Println(output)
-
-		if err != nil {
-			resp, err := utils.SendTextToTelegram(update.CallbackQuery.From.ID, output, keyboard)
-			if err != nil {
-				log.Printf("got error %s from telegram, response body is %s", err.Error(), resp)
-			} else {
-				log.Printf("punchline %s successfully distributed to chat id %d", output, update.CallbackQuery.From.ID)
-			}
-			return
-		}
-	} else if callbackData == "BN" {
-		output, err = utils.GetNewsForResponse("business-insider")
-		if err != nil {
-			resp, err := utils.SendTextToTelegram(update.CallbackQuery.From.ID, output, keyboard)
-			if err != nil {
-				log.Printf("got error %s from telegram, response body is %s", err.Error(), resp)
-			} else {
-				log.Printf("punchline %s successfully distributed to chat id %d", output, update.CallbackQuery.From.ID)
-			}
-			return
-		}
-	} else if callbackData == "TN" {
-		output, err = utils.GetNewsForResponse("techcrunch")
-		if err != nil {
-			resp, err := utils.SendTextToTelegram(update.CallbackQuery.From.ID, output, keyboard)
-			if err != nil {
-				log.Printf("got error %s from telegram, response body is %s", err.Error(), resp)
-			} else {
-				log.Printf("punchline %s successfully distributed to chat id %d", output, update.CallbackQuery.From.ID)
-			}
-			return
-		}
-	} else {
-		// No callbacks hence start looking for commands in user inputs
-		if userCmd == "/start" {
-			output = "Hello I'm " + types.BotName + " I can do the following things for you \n\n"
-			for command, desc := range types.Commands {
-				output += command + " : " + desc + "\n"
-			}
-		} else if userCmd == "/news" {
-			but := types.Buttons{}
-			but.CreateInlineButtons(1, 3, "General News", "GN", "Business News", "BN", "Tech News", "TN")
-
-			keyboard, err = json.Marshal(but)
-			if err != nil {
-				log.Printf(err.Error())
-				return
-			}
-
-			output += "Great.. Almost there.. Please choose which kind of news you want\n"
-		} else if userCmd == "/word" {
-			output, err = utils.GetWordOfTheDay()
-			if err != nil {
-				resp, err := utils.SendTextToTelegram(update.Message.Chat.ID, output, keyboard)
-				if err != nil {
-					log.Printf("got error %s from telegram, response body is %s", err.Error(), resp)
-				} else {
-					log.Printf("punchline %s successfully distributed to chat id %d", output, update.Message.Chat.ID)
-				}
-				return
-			}
-		}
-		// else if userCmd == "/port" {
-		// 	output = "Sorry... This command is under implementation..."
-		// }
-	}
-
-	var resp string
-	if update.Message.Chat.ID != 0 {
-		resp, err = utils.SendTextToTelegram(update.Message.Chat.ID, output, keyboard)
-	} else if update.CallbackQuery.From.ID != 0 {
-		resp, err = utils.SendTextToTelegram(update.CallbackQuery.From.ID, output, keyboard)
-	}
-
-	if err != nil {
-		log.Printf("got error %s from telegram, response body is %s", err.Error(), resp)
-	} else {
-		log.Printf("punchline %s successfully distributed tooooo chat id %d", output, update.Message.Chat.ID)
-	}
-}
-
 func main() {
-	log.Printf(os.Getenv("PORT"))
-	http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), http.HandlerFunc(handler))
+	bot, err := tgbotapi.NewBotAPI("1813786957:AAGj09IMnW05i-5Q3Wik3jsEkdOvbd3Ziag")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
